188eab78cd10d78619edd0d81a69254bfa06c932
EC2 Default User <ec2-user@ip-172-31-10-14.us-east-2.compute.internal>
2025-07-27 09:31:20 +0000

    Challenge 2.2 Modularized the EC2 instance profile

--- main.tf
+++ main.tf
@@ -42,51 +42,26 @@ resource "aws_dynamodb_table" "products_table" {
   }
 }
 
-data "aws_iam_policy_document" "ec2_assume_role_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+module "ec2_instance_profile" {
+  source = "./modules/ec2_instance_profile"
+  profile_name = "jamby-terra"
+  iam_policies = [
+    {
+      effect = "Allow",
+      actions = [
+        "dynamodb:PutItem",
+        "dynamodb:Scan"
+      ]
+      resources = [
+        aws_dynamodb_table.products_table.arn
+      ]
     }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "ec2_role" {
-  name               = "${local.team_name}-ec2-role"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "dynamodb_access_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "dynamodb:PutItem",
-      "dynamodb:Scan"
-    ]
-    resources = [
-      aws_dynamodb_table.products_table.arn
-    ]
-  }
-}
-
-resource "aws_iam_role_policy" "dynamodb_access" {
-  name   = "${local.team_name}-dynamodb-access"
-  role   = aws_iam_role.ec2_role.id
-  policy = data.aws_iam_policy_document.dynamodb_access_policy.json
-}
-
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "${local.team_name}-ec2-profile"
-  role = aws_iam_role.ec2_role.name
+  ]
 }
 
 module "ec2_web_instance" {
   source = "./modules/ec2"
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  iam_instance_profile = module.ec2_instance_profile.profile_name
   keypair_name = aws_key_pair.deployer.key_name
   ec2_instance_name = "jamby-webserver"
   user_data = <<-EOF
