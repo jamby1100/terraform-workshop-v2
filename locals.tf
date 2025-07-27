596b2510fcecc6f767fe3a3f0583d430f8d4b9e8
EC2 Default User <ec2-user@ip-172-31-10-14.us-east-2.compute.internal>
2025-07-27 05:32:41 +0000

    Challenge 1.3 - Added a basic Python application behind Nginx proxy

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+    # my team name
+    team_name = "jamby"
+    flask_github_repo = "https://github.com/jamby1100/flask-app-for-terraform-workshop.git"
+    assigned_aws_region = "us-east-2"
+}
